9618655f440e86d9a6a1e5d6ac221f9d6502566c
i-am-phoenix <iryna.p.marchiano@gmail.com>
2021-07-30 10:06:51 -0500

    Optimized code for parcing the data and simultaneous hirizontal bar chart drafting

--- StarterCode/app.js
+++ StarterCode/app.js
@@ -5,72 +5,9 @@ console.log(Object.keys(data1))
 console.log(data1.samples)
 
 let samples_data = data1.samples
-console.log(samples_data)
+console.log(`Number of test subjects ${samples_data.length}`)
 
-myID = samples_data.map(function (row){
-    addSelection(row.id);
-    return row.id
-  });
-
-  console.log(myID)
-
-// Read in sample id
-let samp_id = samples_data.map(function(row) {
-    addSelection(row.id);
-    return row.id;
-    });
-
-    // console.log("Samp id:", samp_id);
-
-// Read in sample values for each respective id
-let samp_val = samples_data.map(function(row) {
-    return row.sample_values;
-    });
-
-    console.log("Samp val:", samp_val);
-    console.log("Samp val:", samp_val[0]);
-
-// Read in otu_ids as labels for the chart
-let otu_id = samples_data.map(function(row) {
-    return row.otu_ids;
-    });
-
-//     console.log("OTU id:", otu_id);
-
-// Read in otu_ids as labels for the chart
-let otu_lbl = samples_data.map(function(row) {
-    return row.otu_labels;
-    });
-
-//     console.log("OTU :", otu_lbl);
-
-// Function to filter out data with specific sample id
-
-let sID = 940
-
-// Initialize
-var plot_samp_val;
-var plot_otu_lbl;
-var plot_otu_id;
-
-for (i=0;i<samp_id.length;i++) {
-    
-    if (parseInt(samp_id[i]) == sID) {
-        console.log("i=",i)
-        
-        plot_otu_lbl=otu_lbl[i];
-        plot_otu_id=otu_id[i];
-        plot_samp_val=samp_val[i];
-        
-        console.log(sID, "| otu_id | ", plot_otu_id);
-        console.log(sID, "| otu_lbl | ", plot_otu_lbl);
-        console.log(sID, "| samp_val | ", plot_samp_val);
-    }
-};
-
-console.log(sID, ">>>>>>>>>>> ", plot_samp_val)
-
-// Function to add sample ID selections
+// Function to add sample ID selections as HTML element
 function addSelection(id) {
     var selection = document.getElementById("selDataset");
     var option = document.createElement("option");
@@ -78,33 +15,44 @@ function addSelection(id) {
     selection.appendChild(option);
   } ;
 
-let trace_hbar = {
-      x: plot_samp_val,
-      y: plot_otu_id,
-      text: plot_otu_lbl,
-      type: 'bar',
-      name: `Subject ID #${sID}`,
-      orientation: 'h'
+// Read in sample ids and create drop-down selector
+for (i=0; i<samples_data.length; i++) {
+    let individualSample = samples_data[i];
+    addSelection(individualSample.id);
 };
 
-let data_hbar = [trace_hbar];
 
- // Apply a title to the layout
- let layout = {
-    title: `Top 10 OTUs found in the individual with Subject ID #${sID}`,
-    width: 300,
-    height: 500//,
-    // margin: {
-    //   l: 100,
-    //   r: 100,
-    //   t: 100,
-    //   b: 100
-    // }
+for (i=0; i<samples_data.length; i++) {
+    individualSample = samples_data[i];
+    console.log(JSON.stringify(individualSample, null, 2))
+    console.log(individualSample.id);
+    console.log(individualSample.otu_ids);
+    console.log(individualSample.otu_labels);
+    console.log(individualSample.sample_values);
+
+    // let sortedSubjectData = individualSample.sort((a, b) => b.sample_values - a.sample_values);
+    let topTenOTU = individualSample;//sortedSubjectData.slice(0,10);
+
+    console.log(`Subject ID #${topTenOTU.id}`);
+
+    let trace_hbar = {
+        x: topTenOTU.sample_values,
+        y: topTenOTU.otu_ids,
+        text: topTenOTU.otu_labels,
+        type: 'bar',
+        name: `Subject ID #${topTenOTU.id}`,
+        orientation: 'h'
   };
-
-  Plotly.newPlot('bar', data_hbar, layout)
-
   
-  // Render the plot to the div tag with id "plot"
-  // Note that we use `traceData` here, not `data`
-  Plotly.newPlot("plot", traceData, layout);
+  let data_hbar = [trace_hbar];
+  
+   // Apply a title to the layout
+   let layout = {
+      title: `Top 10 OTUs<br>found in Subject ID #${topTenOTU.id}`,
+      width: 300,
+      height: 500
+    };
+  
+    Plotly.newPlot('bar', data_hbar, layout)
+    // break; 
+}
